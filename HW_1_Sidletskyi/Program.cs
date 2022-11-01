46658d0bad0f54f3b7907c2f7d4e275b4c5cf4e9
senivlm <91743906+senivlm@users.noreply.github.com>
2022-11-01 14:46:06 +0200

    Update Program.cs

--- HW_1_Sidletskyi/Program.cs
+++ HW_1_Sidletskyi/Program.cs
@@ -1,9 +1,10 @@
 ﻿using HW_1_Sidletskyi;
-
+// Вітаю. Все доступно.
 Product product = new Product("phone", 399.99m, 0.230);
 
 Buy cart = new Buy(product, 3);
 
 Check check = new Check();
+// Дублюється інформація про продукт. Він вже є в cart
 Console.WriteLine(check.CreateCheck(product, cart));
 Console.WriteLine("-------------------------------------------");
